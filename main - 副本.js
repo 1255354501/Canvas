a82cda70e711616d72b55119fd9ae3adb21ad4cd
xiaot <xiaot@cbpmgw.cn>
2016-05-06 14:27:46 +0800

    commit

---
+++ main - 副本.js
@@ -0,0 +1,102 @@
+var canvas,context;
+
+var img,//图片对象
+    imgIsLoaded,//图片是否加载完成;
+    imgX=0,
+    imgY=0,
+    imgScale=1;
+
+var ZoomRate=2;
+
+var LoadImageName;
+
+(function int(){
+    canvas=document.getElementById('canvas');
+    context=canvas.getContext('2d');
+    loadImg("3.bmp");
+})();
+
+function loadImg(ImageName){
+    img=new Image();
+    img.onload=function(){
+    imgIsLoaded=true;
+    drawImage();
+    }
+    img.src=ImageName;
+}
+
+function drawImage(){
+    context.clearRect(0,0,canvas.width,canvas.height);
+    context.drawImage(img,0,0,img.width,img.height,imgX,imgY,img.width*imgScale,img.height*imgScale);
+    
+    //context.fillRect(0, 0, 100*imgScale, 100*imgScale);
+}
+
+canvas.onmousedown=function(event){
+    
+    var pos=windowToCanvas(canvas,event.clientX,event.clientY);
+    
+	//鼠标按下左键
+	canvas.onmousemove=function(event){
+        canvas.style.cursor="move";
+        var pos1=windowToCanvas(canvas,event.clientX,event.clientY);
+        var x=pos1.x-pos.x;
+        var y=pos1.y-pos.y;
+        pos=pos1;
+        imgX+=x;
+        imgY+=y;
+        drawImage();
+    }
+	
+	
+	//鼠标松开左键
+    canvas.onmouseup=function(){
+        canvas.onmousemove=null;
+        canvas.onmouseup=null;
+        canvas.style.cursor="default";
+    }
+}
+
+
+
+
+
+
+//图像缩放,最大缩放40倍
+canvas.onmousewheel=canvas.onwheel=function(event){
+    var pos=windowToCanvas(canvas,event.clientX,event.clientY);
+    //alert(event.clientX+"-"+event.clientY+"-"+event.wheelDelta);
+    
+    //event.wheelDelta=event.wheelDelta?event.wheelDelta:(event.deltaY*(-40));  
+    
+    if(event.wheelDelta>0){
+        imgScale*=ZoomRate;
+        imgX=imgX*ZoomRate-pos.x;
+        imgY=imgY*ZoomRate-pos.y;
+    }else{
+        imgScale/=ZoomRate;
+        imgX=imgX*(1/ZoomRate)+pos.x*(1/ZoomRate);
+        imgY=imgY*(1/ZoomRate)+pos.y*(1/ZoomRate);
+    }
+    drawImage();
+}
+
+function windowToCanvas(canvas,x,y){
+    var bbox = canvas.getBoundingClientRect();
+    
+    return {
+        x:x - bbox.left - (bbox.width - canvas.width) / ZoomRate,
+        y:y - bbox.top - (bbox.height - canvas.height) / ZoomRate
+    };
+}
+
+function InitFromImageFromFile(imageName)
+{
+	LoadImageName=imageName;
+	loadImg(imageName);
+}
+
+function InitFromBuffer(imageData,height,width)
+{
+
+}
